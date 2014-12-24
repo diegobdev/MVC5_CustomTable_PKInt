5f13f1774694bea8c2bb781883463107182fb4e2
Diego Borges (diegobdev) <dborges.ti@gmail.com>
2014-12-23 22:00:58 -0200

    criação do projeto

---
+++ MVC5_CustomTable_PKInt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5_CustomTable_PKInt.MvcApplication" Language="C#" %>
---
+++ MVC5_CustomTable_PKInt/Models/IdentityModels.cs
@@ -0,0 +1,76 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MVC5_CustomTable_PKInt.Models
+{
+    public class UserRoleIntPk : IdentityUserRole<int>
+    {
+    }
+
+    public class UserClaimIntPk : IdentityUserClaim<int>
+    {
+    }
+
+    public class UserLoginIntPk : IdentityUserLogin<int>
+    {
+    }
+
+    public class RoleIntPk : IdentityRole<int, UserRoleIntPk>
+    {
+        //public RoleIntPk() { }
+        //public RoleIntPk(string name) { Name = name; }
+    }
+
+    public class UserStoreIntPk : UserStore<ApplicationUser, RoleIntPk, int,
+        UserLoginIntPk, UserRoleIntPk, UserClaimIntPk>
+    {
+        public UserStoreIntPk(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+
+    public class RoleStoreIntPk : RoleStore<RoleIntPk, int, UserRoleIntPk>
+    {
+        public RoleStoreIntPk(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+
+    public class ApplicationUser : IdentityUser<int, UserLoginIntPk, UserRoleIntPk, UserClaimIntPk>
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(ApplicationUserManager manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, RoleIntPk, int, UserLoginIntPk, UserRoleIntPk, UserClaimIntPk>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>().ToTable("usuarios");
+            modelBuilder.Entity<RoleIntPk>().ToTable("us_regras");
+            modelBuilder.Entity<UserLoginIntPk>().ToTable("us_logins");
+            modelBuilder.Entity<UserRoleIntPk>().ToTable("us_usuarios_funcoes");
+            modelBuilder.Entity<UserClaimIntPk>().ToTable("us_reivindicacoes");
+        }
+    }
+}
